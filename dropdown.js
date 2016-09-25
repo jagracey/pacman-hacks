2f382c211ddd4b3423a600560df1fa9edb007db4
reinhardtcgr <reinhardt.cgr@gmail.com>
2016-09-24 18:08:52 -0700

    Got the rounding to two decimal places to work

--- dropdown.js
+++ dropdown.js
@@ -27,7 +27,7 @@ var handlers = {
     }, 0);
   },
   betPercent: function (bets, ghost) {
-    return ( parseFloat(bets[ghost]) / handlers.totalBets(bets) ) * 100;
+    return ( parseFloat(bets[ghost]) / handlers.totalBets(bets) ).toFixed(2) * 100;
   },
   playerPayout: function (bets) {
     return minBet * handlers.totalBets(bets) / parseFloat(bets[winner]);
